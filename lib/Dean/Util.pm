af26d80ae525ccaf6a8ff7699b9c4785aadc280f
Dean Serenevy <dean@serenevy.net>
2025-07-26 09:11:19 -0400

    Dean::Util 1.089

--- lib/Dean/Util.pm
+++ lib/Dean/Util.pm
@@ -13,7 +13,7 @@ use base "Exporter";
             /;
 
 $EXPORT_TAGS{_fake} = [qw/INCLUDE_POD POD_ONLY/];
-our $VERSION = '1.088';
+our $VERSION = '1.089';
 
 sub INCLUDE_POD { 1 };
 sub POD_ONLY    { 1 };
